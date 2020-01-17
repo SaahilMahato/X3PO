185b838b38192ca65df819443c0d5a4bebf4f56c
SaahilMahato <saahilmahato72@gmail.com>
2020-01-17 15:47:40 +0545

    Created Assistant.py with speak and listen methods

---
+++ Assistant.py
@@ -0,0 +1,30 @@
+import pyttsx3
+import speech_recognition as sr
+
+
+class Character:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        self.recognizer = sr.Recognizer()
+        self.engine.setProperty('rate', 130)
+
+
+class Interact(Character):
+    def __init__(self):
+        super().__init__()
+
+    def speak(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+
+    def listen(self):
+        with sr.Microphone() as source:
+            audio = self.recognizer.listen(source)
+            try:
+                temp_text = self.recognizer.recognize_google(audio)
+                return temp_text
+            except:
+                return "Sorry, I couldn't get what you said."
+
+
+Interact().speak("Hello Saahil, How are you?")
