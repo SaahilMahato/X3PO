22128f3bc411cfc7c605739f8b8606f7d6b4ad28
SaahilMahato <saahilmahato72@gmail.com>
2020-01-22 01:56:16 +0545

    Created change volume and youtube video download tasks

--- Tasks.py
+++ Tasks.py
@@ -1,41 +1,68 @@
-import os
 import Assistant
+import alsaaudio
+import subprocess
+import pytube
+from pytube.exceptions import RegexMatchError
 
 
 class OpenApp(Assistant.Interact):
     def __init__(self):
         super().__init__()
         self.app_dictionary = {
-            "pycharm": "pycharm",
             "vscode": "code",
             "vs code": "code",
             "visual studio code": "code",
             "chromium": "chromium-browser",
-            "geany": "geany",
-            "android studio": "android-studio"
+            "geany": "geany"
         }
 
-    def open(self):
-        app_text = self.listen().lower()
-        assert app_text is not None
+    def open(self, app):
         try:
-            os.system(self.app_dictionary[app_text])
-        except:
+            subprocess.call(self.app_dictionary[app])
+        except ...:
             self.speak("Sorry, the application is not installed")
 
-"""
+
 class SystemControl(Assistant.Interact):
     def __init__(self):
         super().__init__()
+        self.audio_mixer = alsaaudio.Mixer()
         self.volume_value = None
         self.brightness_value = None
 
-    def change_volume(self):
-        
+    def change_volume(self, value):
+        try:
+            assert 0 <= value <= 100
+            self.audio_mixer.setvolume(value)
+        except ...:
+            self.speak("Please install alsa audio to enable this command")
+
+    def mute_audio(self):
+        try:
+            self.audio_mixer.setvolume(0)
+        except ...:
+            self.speak("Please install alsa audio to enable this command")
 
 
+class VideoDownload(Assistant.Interact):
+    def __init__(self):
+        super().__init__()
+
+    def download_video(self):
+        try:
+            self.get_textbox_input("Enter the video URL and press Enter.")
+            yt = pytube.YouTube(self.text_box_input)
+            video = yt.streams.first()
+            self.speak(f"Downloading video {yt.title}.")
+            video.download(self.youtube_download_path)
+            self.speak(f"Download complete.")
+        except RegexMatchError:
+            self.speak("Sorry, incorrect url.")
+        except ConnectionError:
+            self.speak("Sorry, no internet connection.")
+        except ...:
+            self.speak("Sorry, an error occurred.")
 
 
 if __name__ == "__main__":
-    OpenApp().open()
-"""
+    VideoDownload().download_video()
