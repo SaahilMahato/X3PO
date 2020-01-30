90d80832904be8b38e614af4e7f4c17a4be5b0a2
SaahilMahato <saahilmahato72@gmail.com>
2020-01-30 22:28:21 +0545

    updated few tasks

--- Tasks.py
+++ Tasks.py
@@ -9,41 +9,42 @@ from pytube.exceptions import RegexMatchError
 
 
 class OpenApp(Assistant.Interact):
-    def __init__(self):
+    def __init__(self, user_input):
         super().__init__()
         self.app_dictionary = {
             "vscode": "code",
             "vs code": "code",
             "visual studio code": "code",
             "chromium": "chromium-browser",
-            "geany": "geany"
+            "geany": "geany",
+            "android studio": "/home/saahil/android-studio/bin/studio.sh"
         }
+        self.user_input = user_input
 
-    def open(self, app):
+    def open(self):
         try:
-            subprocess.call(self.app_dictionary[app])
-        except ...:
+            subprocess.call(self.app_dictionary[self.user_input])
+        except:
             self.speak("Sorry, the application is not installed")
 
 
 class SystemControl(Assistant.Interact):
-    def __init__(self):
+    def __init__(self, volume_value=0):
         super().__init__()
         self.audio_mixer = alsaaudio.Mixer()
-        self.volume_value = None
-        self.brightness_value = None
+        self.volume_value = volume_value
 
-    def change_volume(self, value):
+    def change_volume(self):
         try:
-            assert 0 <= value <= 100
-            self.audio_mixer.setvolume(value)
-        except ...:
+            assert 0 <= self.volume_value <= 100
+            self.audio_mixer.setvolume(self.volume_value)
+        except:
             self.speak("Please install alsa audio to enable this command")
 
     def mute_audio(self):
         try:
             self.audio_mixer.setvolume(0)
-        except ...:
+        except:
             self.speak("Please install alsa audio to enable this command")
 
 
@@ -78,8 +79,8 @@ class VideoDownload(Assistant.Interact):
             self.speak("Sorry, incorrect url.")
         except ConnectionError:
             self.speak("Sorry, no internet connection.")
-        except ...:
-            self.speak("Sorry, an error occurred.")
+        except:
+            self.speak("Sorry, pytube need to be updated.")
 
 
 class CreateProject(Assistant.Interact):
@@ -87,20 +88,24 @@ class CreateProject(Assistant.Interact):
         super().__init__()
         self.root = tk.Tk()
         self.project_name_text_box = tk.Text(self.root, height=1, width=30)
+        self.project_files_name_text_box = tk.Text(self.root, height=1, width=80)
         self.remote_url_text_box = tk.Text(self.root, height=1, width=80)
         self.project_name_text_box_input = None
+        self.project_files_name_text_box_input = None
         self.remote_url_text_box_input = None
         self.done_button = tk.Button(self.root, text="Done", command=self.set_user_input)
         self.project_path = "/home/saahil/"
 
     def set_user_input(self):
         self.project_name_text_box_input = self.project_name_text_box.get("1.0", 'end-1c')
+        self.project_files_name_text_box_input = self.project_files_name_text_box.get("1.0", 'end-1c')
         self.remote_url_text_box_input = self.remote_url_text_box.get("1.0", 'end-1c')
         self.root.destroy()
 
     def get_user_input(self):
         self.root.title("Enter project name and git url")
         self.project_name_text_box.pack()
+        self.project_files_name_text_box.pack()
         self.remote_url_text_box.pack()
         self.done_button.pack()
         tk.mainloop()
@@ -112,14 +117,12 @@ class CreateProject(Assistant.Interact):
             assert re.match('((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?', self.remote_url_text_box_input)
             os.system(f'mkdir {self.project_path}{self.project_name_text_box_input}')
             os.chdir(f'{self.project_path}{self.project_name_text_box_input}')
+            os.system('git init')
             os.system('git config --global user.name "SaahilMahato"')
             os.system('git config --global user.email "saahilmahato72@gmail.com"')
-            os.system('git init')
             os.system(f'git remote add origin {self.remote_url_text_box_input}')
-            os.system(f'git pull origin master')
-        except ...:
-            self.speak("Sorry, could not create project")
-
-
-if __name__ == "__main__":
-    CreateProject().create_project()
+            for file in self.project_files_name_text_box_input.split(","): os.system(f"touch {file}")
+        except AssertionError:
+            self.speak("Sorry, invalid git URL or project name")
+        except:
+            self.speak("Sorry, an error occurred")
