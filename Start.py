457883f16469ad2da7adf234a92e111b0d2c98cb
SaahilMahato <saahilmahato72@gmail.com>
2020-01-31 12:08:27 +0545

    used OOP

--- Start.py
+++ Start.py
@@ -1,15 +1,27 @@
 import Assistant
 import DeduceInput
 
+
+class Start(Assistant.Interact):
+    def __init__(self):
+        super().__init__()
+        self.trigger_word = "hello"
+        self.shut_down_word = "goodbye"
+
+    def start(self):
+        while True:
+            user_input_trigger = self.listen()
+            if user_input_trigger == self.shut_down_word:
+                self.speak("I am shutting down.")
+                exit()
+            if user_input_trigger == self.trigger_word:
+                self.speak("Hi, How can I help you?")
+                user_query = self.listen()
+                if user_query is not None:
+                    DeduceInput.DeduceInput(user_query).deduce()
+                else:
+                    self.speak("Sorry, I could not get you.")
+
+
 if __name__ == '__main__':
-    trigger_word = "hello"
-    x3po = Assistant.Interact()
-    while True:
-        user_input_trigger = x3po.listen()
-        if user_input_trigger == "hello":
-            x3po.speak("Hi, How can I help you?")
-            user_query = x3po.listen()
-            if user_query is not None:
-                DeduceInput.DeduceInput(user_query).deduce()
-            else:
-                x3po.speak("Sorry, I could not get you.")
+    Start().start()
